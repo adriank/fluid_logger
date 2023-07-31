9ebd6d1a175d032987f685e0eb039efa27d0a647
Adrian Kalbarczyk <adrian.kalbarczyk@gmail.com>
2023-07-31 20:54:04 +0200

    enhacements & web support

--- lib/src/fluid_logger.dart
+++ lib/src/fluid_logger.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: avoid_print
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
@@ -6,8 +7,8 @@ import 'package:flutter/foundation.dart';
 import 'package:fluid_logger/src/colorify.dart';
 
 @immutable
-class LogMessage {
-  const LogMessage({
+class LogMessageData {
+  const LogMessageData({
     required this.timestamp,
     required this.track,
     required this.fileRelativePath,
@@ -30,7 +31,9 @@ class LogMessage {
   final int columnNumber;
 }
 
-String defaultMessageFormatter(LogMessage message, LogMessage? previousLogMessage) => '[${message.track}] ${message.timestamp.toString().split(' ')[1]} fn:${message.functionName} ${message.fileLink}\n${message.message}';
+String defaultMessageFormatter(LogMessageData message, LogMessageData? previousLogMessage) => '''
+[${message.track}] ${message.timestamp.toString().split(' ')[1]} fn:${message.functionName} ${message.fileLink}
+[${message.track}] ${message.message}''';
 
 enum DebugLevel {
   debug,
@@ -74,6 +77,8 @@ class FluidLogger {
     this.packageName = 'lib',
   });
 
+  static String pretty(dynamic object, {int indent = 2}) => JsonEncoder.withIndent(' ' * indent).convert(json);
+
   /// Provide package name from pubspec.yaml to get relative files paths.
   final String packageName;
 
@@ -84,11 +89,11 @@ class FluidLogger {
 
   /// Set non-standard message formatter globally. Default is defined in [defaultMessageFormatter].
   /// WARNING! The line that contains fileLink shouldn't be too long. Otherwise console will not generate a clickable link.
-  static const String Function(LogMessage message, LogMessage? previousLogMessage) globalMessageFormatter = defaultMessageFormatter;
+  static const String Function(LogMessageData message, LogMessageData? previousLogMessage) globalMessageFormatter = defaultMessageFormatter;
 
   /// Set non-standard message formatter for this [track]. Default is defined in [defaultMessageFormatter].
   /// WARNING! The line that contains fileLink shouldn't be too long. Otherwise console will not generate a clickable link.
-  final String Function(LogMessage message, LogMessage? previousLogMessage) messageFormatter;
+  final String Function(LogMessageData message, LogMessageData? previousLogMessage) messageFormatter;
 
   /// Used to distinguish between different loggers. E.g. 'Routing', 'UserRepository'.
   final String? track;
@@ -100,27 +105,28 @@ class FluidLogger {
     if (!shouldPrintDebug(level)) {
       return;
     }
-    if (kIsWeb) {
-      _printWeb();
-      return;
-    }
+
     final (trace, previousTrace) = _getTraceData();
-    final message = level == DebugLevel.start ? '${trace.functionName}(${messageFn()})' : messageFn();
-    final shouldCut = cutAfter == null ? false : cutAfter! < message.length;
-    final logMessage = LogMessage(
+    // print(trace);
+    final message = switch (level) {
+      DebugLevel.start => '${trace.functionName}(${messageFn()})',
+      _ => messageFn(),
+    };
+    final shouldCut = cutAfter != null && cutAfter! < message.length;
+    final logMessage = LogMessageData(
       timestamp: DateTime.now(),
       track: track,
       fileRelativePath: (trace.fileName.split(packageName)..removeAt(0)).join(packageName),
       fileLink: trace.link,
-      functionName: '${trace.functionName}()',
+      functionName: trace.functionName,
       lineNumber: trace.lineNumber,
       columnNumber: trace.columnNumber,
-      message: ((shouldCut ? '${message.substring(0, cutAfter)}...(cut)' : message)).trim(),
+      message: (shouldCut ? '${message.substring(0, cutAfter)}...(cut)' : message).trim(),
       level: level,
     );
     final previousLogMessage = previousTrace == null
         ? null
-        : LogMessage(
+        : LogMessageData(
             timestamp: DateTime.now(),
             track: track,
             fileRelativePath: (previousTrace.fileName.split(packageName)..removeAt(0)).join(packageName),
@@ -128,7 +134,7 @@ class FluidLogger {
             functionName: previousTrace.functionName,
             lineNumber: previousTrace.lineNumber,
             columnNumber: previousTrace.columnNumber,
-            message: ((shouldCut ? '${message.substring(0, cutAfter)}...(cut)' : message)).trim(),
+            message: (shouldCut ? '${message.substring(0, cutAfter)}...(cut)' : message).trim(),
             level: level,
           );
     final toPrint = messageFormatter(logMessage, previousLogMessage);
@@ -137,43 +143,32 @@ class FluidLogger {
       for (var e in toPrint.split('\n')) {
         if (e.contains(logMessage.fileLink)) {
           e = e.replaceAll(logMessage.fileLink, '');
-          if (e.length + logMessage.fileLink.length > 120) {
-            log(
-              level.colorify(e),
-              level: 0,
-              name: level.toString(),
-            );
-            log(
-              logMessage.fileLink,
-              level: 0,
-              name: level.toString(),
-            );
-            continue;
-          }
+          // if (e.length + logMessage.fileLink.length > 1500) {
+          //   log(
+          //     level.colorify(e),
+          //     level: 0,
+          //     name: level.toString(),
+          //   );
+          //   log(
+          //     logMessage.fileLink,
+          //     level: 0,
+          //     name: level.toString(),
+          //   );
+          //   continue;
+          // }
           log(
             '${level.colorify(e)} ${logMessage.fileLink}',
-            level: 0,
             name: level.toString(),
           );
           continue;
         }
         log(
           level.colorify(e),
-          level: 0,
           name: level.toString(),
         );
       }
     } else {
       print(toPrint);
-    }
-  }
-
-  void _printWeb() {
-    try {
-      throw Exception();
-    } catch (e, s) {
-      print(e);
-      print(s);
     }
   }
 
@@ -186,62 +181,78 @@ class FluidLogger {
   }
 
   /// Print information about current function. Takes a list of function arguments. The log message level is 'info'.
-  start([List<dynamic>? arguments]) => _print(
-        () => (arguments ?? []).join(', '),
+  void start([List<dynamic>? arguments]) => _print(
+        () => (arguments ?? []).map((e) => e.toString()).join(', \n'),
         level: DebugLevel.start,
       );
 
   /// The most granular messages. Should be printed only when feature needs to be thoughtfully debugged.
-  debug(String Function() message) => _print(message, level: DebugLevel.debug);
+  void debug(String Function() message) => _print(message);
 
   /// Information that is more general than debug level.
-  info(String Function() message) => _print(message, level: DebugLevel.info);
+  void info(String Function() message) => _print(message, level: DebugLevel.info);
 
   /// Message that requires attention of a developer.
-  warning(String Function() message) => _print(message, level: DebugLevel.warning);
+  void warning(String Function() message) => _print(message, level: DebugLevel.warning);
 
   /// Indicates success of an operation. Should be used after successful data retrieval from server, database etc.
-  success(String Function() message) => _print(message, level: DebugLevel.success);
+  void success(String Function() message) => _print(message, level: DebugLevel.success);
 
   /// Indicates error of an operation. Should be used after unsuccessful data retrieval from server, database etc.
-  error(String Function() message) => _print(message, level: DebugLevel.error);
+  void error(String Function() message) => _print(message, level: DebugLevel.error);
 
   /// Whether log message. Checks for forceDebugMessages, kDebugMode, and log level.
   bool shouldPrintDebug(DebugLevel level) => forceDebugMessages || kDebugMode && DebugLevel.values.indexOf(level) >= DebugLevel.values.indexOf(debugLevel);
 }
 
+@immutable
 class _Frame {
-  _Frame({
+  const _Frame({
     required this.fileName,
     required this.link,
     required this.functionName,
     required this.columnNumber,
     required this.lineNumber,
   });
-  late String fileName;
-  late String link;
-  late String functionName;
-  late int lineNumber;
-  late int columnNumber;
+
+  final String fileName;
+  final String link;
+  final String functionName;
+  final int lineNumber;
+  final int columnNumber;
+
+  @override
+  String toString() => '''
+fileName: $fileName
+link: $link
+functionName: $functionName
+lineNumber: $lineNumber
+columnNumber: $columnNumber
+''';
 }
 
 class _CustomTrace {
-  _Frame currentCall;
-  _Frame? previousCall;
-
   _CustomTrace({required this.currentCall, this.previousCall});
 
   factory _CustomTrace.fromStackTrace(StackTrace trace) {
+    // print('start _CustomTrace.fromStackTrace');
     final frames = trace.toString().split('\n');
+    // print('frames:');
+    // frames.forEach(print);
+    const startIndex = kIsWeb ? 4 : 3;
     return _CustomTrace(
-      currentCall: _readFrame(frames[3]),
-      previousCall: (frames.length >= 4) ? _readFrame(frames[4]) : null,
+      currentCall: _readFrame(frames[startIndex]),
+      previousCall: (frames.length >= startIndex + 1) ? _readFrame(frames[startIndex + 1]) : null,
     );
   }
 
+  _Frame currentCall;
+  _Frame? previousCall;
+
   static _Frame _readFrame(String frame) {
+    // print('start _CustomTrace._readFrame, $frame');
     if (frame == '<asynchronous suspension>') {
-      return _Frame(
+      return const _Frame(
         fileName: '?',
         link: '',
         lineNumber: 0,
@@ -249,12 +260,25 @@ class _CustomTrace {
         columnNumber: 0,
       );
     }
-    final parts = frame.replaceAll(r'<anonymous closure>', 'anonymous').replaceAll(r'<anonymous, closure>', 'anonymous').split(' ').where((element) => element.isNotEmpty && element != 'new').toList();
+    final parts = frame.replaceAll('<anonymous closure>', 'anonymous').replaceAll('<anonymous, closure>', 'anonymous').split(' ').where((element) => element.isNotEmpty && element != 'new').toList();
+    // print(parts);
+
+    if (kIsWeb) {
+      return _Frame(
+        fileName: parts[0],
+        link: parts[0],
+        lineNumber: int.parse(parts[1].split(':')[0]),
+        functionName: parts[2].split('.anony')[0].split('(')[0].replaceAll('<fn>', 'anonymous fn'),
+        columnNumber: int.parse(parts[1].split(':')[1]),
+      );
+    }
     List<String> listOfInfos = ['', '', '0', '0'];
-    // log('${parts[1]} ${parts[2]} ');
+
+    // print(listOfInfos);
     try {
       listOfInfos = parts[2].split(':');
     } catch (_) {}
+
     return _Frame(
       fileName: listOfInfos[1],
       link: parts[2],
